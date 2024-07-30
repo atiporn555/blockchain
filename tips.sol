58d6d03384be594d99330efbe124904d2012534d
atiporn555 <653070310129@npu.ac.th>
2024-07-30 15:24:36 +0700

    สร้างฟังชั่นเติมเงิน/ดูยอดเงิน

--- tips.sol
+++ tips.sol
@@ -8,9 +8,23 @@ contract tips {
     }
 
     //1. ใส่เงินเข้าไปในกระปุก
+
+    function addtips() payable public {
+        
+        
+    }
+
     //2. เช็คว่ามีเงินในกระปุกเท่าไหร่
+
+    function viewtips() public view returns (uint){
+        return address(this).balance;
+    }
+
+    
+    
+    
     //3. แอด userคือเพิ่มคนที่จะได้รับติ้บ
     //4. ลบ user
     //5. ดู user
     //6. จ่ายเงิน
-}
+} 
