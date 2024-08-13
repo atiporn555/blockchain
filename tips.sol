a94af0cdf703205c6c756ab369a04e93e8e78efe
atiporn555 <653070310129@npu.ac.th>
2024-08-13 15:27:57 +0700

    add waitress

--- tips.sol
+++ tips.sol
@@ -2,7 +2,10 @@
 pragma solidity 0.8.26;
 
 contract tips {
+
     address owner;
+    Waitress[] waitress;
+    
     constructor(){
         owner = msg.sender;
     }
@@ -22,9 +25,37 @@ contract tips {
 
     
     
-    
-    //3. แอด userคือเพิ่มคนที่จะได้รับติ้บ
-    //4. ลบ user
-    //5. ดู user
-    //6. จ่ายเงิน
-} 
+    //3. add structure waitress คือเพิ่มคนที่จะได้รับติ้บ
+            struct Waitress{
+                address payable walletAddress;
+                string name;
+            }
+
+    //3.2 add waitress
+    function addWaitress(address payable walletAddress,string memory name) public {
+        bool waitressExist = false;
+
+        if (waitress.length >=1){
+             for(uint i=0; i<waitress.length; i++){
+           if(waitress[i].walletAddress == walletAddress)
+           waitressExist = true;
+
+        }
+       
+        }
+  
+        
+
+        if(waitressExist==false){
+            waitress.push(Waitress(walletAddress,name));
+        }
+    }
+ //4. ลบ user
+
+    //5. ดู waitress
+    function viewWaitress() public view returns (Waitress[] memory) {
+        return waitress; 
+    }
+
+     //6. จ่ายเงิน
+     } 
